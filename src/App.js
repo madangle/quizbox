772117293910fb2546f57d8402e615d26bc2491d
Rishi <rishi@Major-Toms-Macbook-Pro.local>
2022-04-21 01:56:31 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [question, setQuestion] = useState(false);
+  const [isAnswerShown, setShowAnswer] = useState(false);
+  const [isNextButtonShown, setShowNextButton] = useState(false);
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      withCredentials: false,
+      url: 'https://coddets.com/api/quizbox/index.php'
+    })
+    .then(res => {
+      const question = res.data;
+      // console.log(question, question.question_text);
+      setQuestion(question);
+    });
+  }, [])  
+
+  const showAnswer = () => {
+    setShowAnswer(true);
+    setShowNextButton(true);
+    console.log(isAnswerShown);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className='Question-Text'>{question.question_text}</p>
+        <button 
+          className='Answer-Button'
+          onClick={showAnswer}
+        >Answer</button>
+
+        { isAnswerShown ? <p className='Answer-Text'>{question.answer_text}</p> : null}
+        
+        <button 
+          className='Next-Button'
+          onClick={setQuestion}
+        >Next</button>
+
       </header>
     </div>
   );
